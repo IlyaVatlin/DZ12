32195ab403c114276c292ec06cdbf8ebfa564389
IlyaVatlin <107053785+IlyaVatlin@users.noreply.github.com>
2022-07-05 16:35:05 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int dengi = 1000;
+        int percent = 20;
+        int bonus = dengi / percent;
+        System.out.println("Ваш бонус: " + bonus + " miles");
+    }
 }
